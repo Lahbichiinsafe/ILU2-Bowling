d4d7d84705e9591c6b16fd42027463a729af8766
Lahbichi <lahbichiinsaf@gmail.com>
2024-04-11 17:29:45 +0200

    test4

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -21,39 +21,29 @@ class GameTest {
     	assertEquals(0, game.score());
     }
     
-   
-
 	@Test
 	void test_aucune_quille(){
 		for (int i = 0 ; i < 20 ; i++)
 			game.roll(0);
 		assertEquals(0, game.score());
-	}
-
-	
-	
+	}	
 	
 	@Test
 	void test_vinght_un() {
 		for(int i = 0 ; i<20 ; i++)
 			game.roll(1);
 		assertEquals(20, game.score());
+	}	
+	
+	@Test
+	void test_dix_un_dix_deux() {
+		for(int i = 0 ; i<10 ; i++) {
+		    	game.roll(1);
+		    	game.roll(2); 
+		}
+		    
+		assertEquals(30, game.score());
 	}
-	
-	
-	
-//	@Test
-//	void test_dix_un_dix_deux() {
-//		for(int i = 0 ; i<20 ; i++)
-//			game.roll(1);
-//		
-//		for(int j=0 ; j<10 ; j++)
-//			game.roll(2);
-//		
-//		
-//		
-//		assertEquals(30, game.score());
-//	}
 	
 
 }

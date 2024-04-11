4fff2591b66e6ad75886f1f2bdd28a6ccc25f7cd
Lahbichi <lahbichiinsaf@gmail.com>
2024-04-11 17:17:01 +0200

    test1

---
+++ src/tdd/Game.java
@@ -0,0 +1,43 @@
+package tdd;
+
+
+public class Game {
+	
+	
+	
+	static int Nb_Max_Tours = 21;
+	private int Rollrecent = 0;
+	private int[] rolls = new int[Nb_Max_Tours];
+
+	
+	
+	
+    void roll(int nbQuilles) {
+        rolls[Rollrecent++] = nbQuilles;
+    }
+	
+    
+    
+    int score() {
+        int scoretotal = 0;
+        int i= 0;
+
+        for (int frame = 0; frame < 10; frame++) {
+                scoretotal += SommeQuilles(i);
+                i += 2;
+            }
+        
+    
+        return scoretotal;
+    }
+
+
+    private int SommeQuilles(int i) {
+        return rolls[i] + rolls[i + 1];
+    }
+}
+	
+
+ 
+
+
---
+++ test/tdd/GameTest.java
@@ -0,0 +1,59 @@
+package tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	
+	Game game;
+	
+    @BeforeEach
+    void init() {
+    	game = new Game();
+    }
+    
+    
+    @Test
+    void test_vide() {
+    	assertEquals(0, game.score());
+    }
+    
+   
+
+//	@Test
+//	void test_aucune_quille(){
+//		for (int i = 0 ; i < 20 ; i++)
+//			game.roll(0);
+//		assertEquals(0, game.score());
+//	}
+//
+//	
+//	
+//	
+//	@Test
+//	void test_vinght_un() {
+//		for(int i = 0 ; i<20 ; i++)
+//			game.roll(1);
+//		assertEquals(20, game.score());
+//	}
+//	
+//	
+//	
+//	@Test
+//	void test_dix_un_dix_deux() {
+//		for(int i = 0 ; i<20 ; i++)
+//			game.roll(1);
+//		
+//		for(int j=0 ; j<10 ; j++)
+//			game.roll(2);
+//		
+//		
+//		
+//		assertEquals(30, game.score());
+//	}
+	
+
+}
